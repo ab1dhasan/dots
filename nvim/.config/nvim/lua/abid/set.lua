32882a86d46a39f06c5ff3224890273d822b715f
Abid Hasan <abid@devsnest.llc>
2024-09-01 19:50:34 +0600

    add treesitter fold

--- nvim/.config/nvim/lua/abid/set.lua
+++ nvim/.config/nvim/lua/abid/set.lua
@@ -7,3 +7,6 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.signcolumn = 'yes'
 vim.o.mouse = ''
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldenable = false
