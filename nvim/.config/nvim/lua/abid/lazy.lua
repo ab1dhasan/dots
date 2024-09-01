e82037b5c7285b15eb4529b127aee63462a93ee4
Abid Hasan <abid@devsnest.llc>
2024-09-01 19:38:06 +0600

    add multi cursor

--- nvim/.config/nvim/lua/abid/lazy.lua
+++ nvim/.config/nvim/lua/abid/lazy.lua
@@ -239,7 +239,10 @@ require("lazy").setup({
 			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
 			{ 'L3MON4D3/LuaSnip' },  -- Required
 			{ 'hrsh7th/cmp-emoji' },
-			{ 'hrsh7th/cmp-buffer' }
+			{ 'hrsh7th/cmp-buffer' },
+			{ 'hrsh7th/cmp-path' },
+			{ 'hrsh7th/cmp-cmdline' },
+			{ 'saadparwaiz1/cmp_luasnip' }
 		}
 	},
 	{
@@ -271,5 +274,6 @@ require("lazy").setup({
 		config = function()
 			require('neoscroll').setup({})
 		end
-	}
+	},
+	{ 'mg979/vim-visual-multi' },
 })
