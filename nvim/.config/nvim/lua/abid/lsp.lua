4387b6e0d5a1cb367adde880d8449e29a9cde999
Abid Hasan <abid@devsnest.llc>
2024-09-01 22:15:03 +0600

    add keymap to scroll doc

--- nvim/.config/nvim/lua/abid/lsp.lua
+++ nvim/.config/nvim/lua/abid/lsp.lua
@@ -93,6 +93,8 @@ cmp.setup({
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<C-k>'] = cmp.mapping.scroll_docs(-4),
+		['<C-j>'] = cmp.mapping.scroll_docs(4),
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
