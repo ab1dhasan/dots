9d5c74674b4b9203e4cb2840e941c5910e28720f
Abid Hasan <abid@hasan.email>
2025-12-24 13:27:12 +0600

    remove dalay

--- nvim/.config/nvim/lua/abid/lazy.lua
+++ nvim/.config/nvim/lua/abid/lazy.lua
@@ -267,7 +267,7 @@ require("lazy").setup({
 				current_line_blame_opts = {
 					virt_text = true,
 					virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-					delay = 1000,
+					delay = 0,
 					ignore_whitespace = false,
 					virt_text_priority = 100,
 				},
