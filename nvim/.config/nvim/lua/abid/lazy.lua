e128824a367fa3f4b48d918b1def9a443fe93f69
Abid Hasan <abid@hasan.email>
2025-12-24 13:25:27 +0600

    remove duplicate line

--- nvim/.config/nvim/lua/abid/lazy.lua
+++ nvim/.config/nvim/lua/abid/lazy.lua
@@ -263,7 +263,7 @@ require("lazy").setup({
 				},
 				auto_attach = true,
 				attach_to_untracked = false,
-				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 				current_line_blame_opts = {
 					virt_text = true,
 					virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
@@ -283,8 +283,7 @@ require("lazy").setup({
 					relative = 'cursor',
 					row = 0,
 					col = 1
-				},
-				current_line_blame = true
+				}
 			}
 		end
 	},
